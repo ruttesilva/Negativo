6063db09125d737cd3fb17e0b3a452bb3ce51178
Rutte Oliveira Silva <rosrosrutte@gmail.com>
2022-06-03 20:36:07 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int numero;
+
+Console.Write ("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine ())!;
+
+if (numero < 0)
+
+{
+    Console.WriteLine("Você digitou um número negativo.");
+}
